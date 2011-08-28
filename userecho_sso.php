7c23e0c37aea95393ee8590faf3ac855c6455bad
Jonathan Champ <jonathan_champ@ncsu.edu>
2011-08-28 10:25:58 -0400

    Fix version, url.

--- userecho_sso.php
+++ userecho_sso.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: UserEcho SSO Plugin
-Version: 1.0.0
-Plugin URI: https://github.com/jrchamp/UserEcho-SSO-Plugin-for-Wordpress
+Version: 0.1
+Plugin URI: https://github.com/jrchamp/userecho_sso
 Author: Jonathan Champ
 Author URI: https://github.com/jrchamp
 Description: Allows users to automatically sign in to the associated UserEcho account.
 */
 
-//add_filter( 'sm_time-text', 'sm_search_label_replace', 100 );
+
