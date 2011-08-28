13b5a549bfc9bff90b824cc6cd93dec4f79411d1
Jonathan Champ <jrchamp@fullthrottleapps.com>
2011-08-28 10:17:33 -0400

    Initial commit

---
+++ userecho_sso.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: UserEcho SSO Plugin
+Version: 1.0.0
+Plugin URI: https://github.com/jrchamp/UserEcho-SSO-Plugin-for-Wordpress
+Author: Jonathan Champ
+Author URI: https://github.com/jrchamp
+Description: Allows users to automatically sign in to the associated UserEcho account.
+*/
+
+//add_filter( 'sm_time-text', 'sm_search_label_replace', 100 );
